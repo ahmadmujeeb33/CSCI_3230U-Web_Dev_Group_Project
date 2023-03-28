58d6806d833fe4201c92abea2ba0c566939c6cda
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2023-03-28 13:27:45 -0400

    ServiceAccountKey.json
    service account key

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <FavoritesList/> -->
+  <AddProductVue/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddProductVue from './components/AddProduct.vue'
+// import FavoritesList from './components/FavoritesList.vue'
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // FavoritesList
+    AddProductVue
   }
 }
 </script>
---
+++ client/src/components/AddProduct.vue
@@ -0,0 +1,48 @@
+
+
+<template >
+  <div>
+    <input type="file" @change="onFileSelected">
+    <button @click="onUpload">Upload</button>
+  </div>
+</template>
+
+<script>
+   import axios from 'axios';
+
+export default {
+  name: 'AddProduct',
+  data (){
+      return{
+         selectedFile:null
+      }
+  },
+  methods:{
+      onFileSelected(event){
+          this.selectedFile = event.target.files[0]
+          console.log(this.selectedFile)
+      },
+
+      async onUpload(event){
+        console.log(event)
+        const formData = new FormData()
+        formData.append('file',this.selectedFile)
+        await axios.post("/api/upload",formData)
+        // let ans = await axios.get("/api")
+        // console.log(ans)
+      }
+  }
+
+  
+ 
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+
+</style>
+ 
