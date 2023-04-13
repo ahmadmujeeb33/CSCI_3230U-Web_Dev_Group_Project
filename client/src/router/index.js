53eb2923195946f2bc30b046dd6ca6a8979763e1
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2023-04-13 13:58:01 -0400

    added code to router and made the code for grabing favorites

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,11 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HelloWorld from '../components/HelloWorld.vue'
+import FavoritesList from '../components/FavoritesList.vue'
+import AddProduct from '../components/AddProduct.vue'
 
 const routes = [
   {
     path: '/',
     name: 'HelloWorld',
     component: HelloWorld
+  },
+
+  // {
+  //   path: '/Favorite',
+  //   name: 'MyFavorites',
+  //   component: MyFavorites
+  // },
+  {
+    path: '/AddProduct',
+    name: 'AddProduct',
+    component: AddProduct
+  },
+
+  {
+    path: '/FavoritesList',
+    name: 'FavoritesList',
+    component: FavoritesList
   },
   
 ]
