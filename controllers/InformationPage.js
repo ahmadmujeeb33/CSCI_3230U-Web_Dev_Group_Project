083ebc306b8804cd1844497b2b4033c1be459c66
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2023-04-18 11:57:56 -0400

    information page updated

--- controllers/InformationPage.js
+++ controllers/InformationPage.js
@@ -1,14 +1,14 @@
 const main = require('../index.js')
 const router = require('express').Router();
 
-router.post('/check_favorites',async(req,res)=>{
+router.get('/check_favorites/:userId/:itemId',async(req,res)=>{
     //   let userId = "dQbBabEpFn87fLxaHTtO"
     //   let itemId = "DNGah477roSUvSbvDw0x"
     console.log("hewwwwwwwwwwwww")
       let favoriteFound  = false
       await main.db.collection('Favorite').get().then((snapshot) => {
         snapshot.forEach((doc) => {
-          if(doc.data()['itemId'].toString() == req.body['itemId'] && doc.data()['userId'].toString() == req.body['userId']){
+          if(doc.data()['itemId'].toString() == req.params.itemId && doc.data()['userId'].toString() == req.params.userId){
             favoriteFound = true
             res.send(true)
           }
@@ -24,7 +24,7 @@ router.post('/check_favorites',async(req,res)=>{
     })
     
 router.post('/add_favorites',async(req,res)=>{
-    console.log(req.body)
+    console.log("add")
 
     let favorite = main.db.collection('Favorite')
     favorite.add(req.body)
@@ -32,15 +32,15 @@ router.post('/add_favorites',async(req,res)=>{
 })
 
 
-router.post('/delete_favorites',async(req,res)=>{
-    console.log(req.body)
+router.delete('/delete_favorites/:userId/:itemId',async(req,res)=>{
+    console.log("delete")
 
     await main.db.collection('Favorite').get().then((snapshot) => {
         snapshot.forEach((doc) => {
-        if(doc.data()['itemId'].toString() == req.body['itemId'] && doc.data()['userId'].toString() == req.body['userId']){
+        if(doc.data()['itemId'].toString() == req.params.itemId  && doc.data()['userId'].toString() == req.params.userId){
             console.log("in heree")
             doc.ref.delete();
-        }
+          }
         });
       })
 
