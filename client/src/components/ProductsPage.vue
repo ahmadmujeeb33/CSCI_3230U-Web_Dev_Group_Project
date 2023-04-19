e01e86b6727cdde6aa0f282caa91fa6250c6ceeb
Samuel Shard <samuel.shard@ontariotechu.net>
2023-04-19 12:28:58 -0400

    Filter working on Products page

--- client/src/components/ProductsPage.vue
+++ client/src/components/ProductsPage.vue
@@ -17,14 +17,6 @@
 				<!-- https://bulma.io/documentation/components/menu/ -->
 				<section class="menu mt-0">
 					<p class="menu-label">
-						Sort
-					</p>
-					<ul class="menu-list">
-						<li><a>By Name</a></li>
-						<li><a>By Price</a></li>
-					</ul>
-
-					<p class="menu-label">
 						Filter
 					</p>
 					<ul class="menu-list">
@@ -32,13 +24,14 @@
 						<li>
 							<a class="is-active">Rating</a>
 							<ul>
-								<li><a>1 Star and Above</a></li>
-								<li><a>2 Star and Above</a></li>
-								<li><a>3 Star and Above</a></li>
-								<li><a>4 Star and Above</a></li>
+								<li><a @click="ratingFilterClick('reviews', 0)">All Ratings</a></li>
+								<li><a @click="ratingFilterClick('reviews', 0.2)">1 Star and Above</a></li>
+								<li><a @click="ratingFilterClick('reviews', 0.4)">2 Star and Above</a></li>
+								<li><a @click="ratingFilterClick('reviews', 0.6)">3 Star and Above</a></li>
+								<li><a @click="ratingFilterClick('reviews', 0.8)">4 Star and Above</a></li>
 							</ul>
 						</li>
-						<li>
+						<li class="categoryMenu">
 							<a class="is-active">Category</a>
 							<ul id="categoryResults">
 							</ul>
@@ -74,21 +67,61 @@
 import store from '../store'
 import $ from 'jquery'
 function productClick(event){
-	console.log("event.target.id",event.target.id);
+	//console.log("event.target.id",event.target.id);
 	store.commit('updateProduct', event.target.id);
 	window.open("/InformationPage", "_self");
 }
-// const products = store.state.message;
-//console.log(products);
+
+// 
+function ratingFilterClick(field, threshold){
+	//show all products (reset and previous filtering)
+	$("#productGrid").find(".card").show();
+	//count how many products are left
+	let numProducts = 0;
+
+	//apply new filter by rating
+	for (var product of store.state.message) {
+		for(var key in product) {
+			// if category string
+			if(typeof product[key][field] === "string"){
+				let category = product[key][field]; //category is from product card
+				if(category !== threshold){
+					$("#productGrid").find("."+key).hide();
+				} else {
+					numProducts += 1;
+				}
+			}
+			// else rating number
+			else{
+				let reviews = product[key][field];
+				let avgRating = 0;
+				let maxPossibleRating = 0
+				for (var i = 0; i < reviews.length; i++) {
+					// # reviews of that rating * review rating
+					avgRating += reviews[i]*i;
+					maxPossibleRating += reviews[i]*5;
+				}
+				avgRating = avgRating / maxPossibleRating;
+				if(avgRating < threshold){
+					$("#productGrid").find("."+key).hide();
+				} else {
+					numProducts += 1;
+				}
+			}
+		}
+	}
+	$("#numProducts").replaceWith('<span id="numProducts">'+numProducts+'</span>');
+}
+
+
 $(document).ready(function () {
-	console.log(store.state.message);
 	let loopCount = 0;
 	let uniqueCategories = [];
 	$("#productGrid").replaceWith("<div class='container' id='productGrid'></div>");
 	for (var product of store.state.message) {
 		for(var key in product) {
 			//add each product to the product grid with the link id set to the product id
-			$("#productGrid").append('<div class="card"><header class="card-header"><p class="card-header-title">'+product[key]["title"]+'</p></header><div class="card-image"><figure class="image is-4by3"><img src="'+product[key]["url"]+'"></figure></div><footer class="card-footer"><a id="'+key+'" class="card-footer-item">Check it out!</a></footer></div>');
+			$("#productGrid").append('<div class="card '+key+'"><header class="card-header"><p class="card-header-title">'+product[key]["title"]+'</p></header><div class="card-image"><figure class="image is-4by3"><img src="'+product[key]["url"]+'"></figure></div><footer class="card-footer"><a id="'+key+'" class="card-footer-item">Check it out!</a></footer></div>');
 			
 			document.getElementById(key).addEventListener("click", productClick); //how to add function parameter?
 
@@ -108,18 +141,22 @@ $(document).ready(function () {
 			if(!(uniqueCategories.includes(product[key]["categories"]))){
 				uniqueCategories.push(product[key]["categories"]);
 			}
-			console.log('product[key]["categories"]',product[key]["categories"]);
 		}
 	}
 	//show number of products
 	$("#numProducts").replaceWith('<span id="numProducts">'+loopCount+'</span>');
-
-	//update category filter with available categories based on search results
-	console.log("uniqueCategories",uniqueCategories);
+	
+	//Show resulting categories
 	$("#categoryResults").replaceWith('<ul id="categoryResults"></ul>');
 	for(var cat in uniqueCategories) {
-		$("#categoryResults").append('<li><a>'+uniqueCategories[cat]+'</a></li>');
+		let currentCat = uniqueCategories[cat];
+		$("#categoryResults").append('<li><a id='+currentCat+'>'+currentCat+'</a></li>');
+
+		document.querySelector('#'+currentCat).addEventListener('click', function(){
+			ratingFilterClick("categories", currentCat);
+		}, false);
 	}
+
 	
 
 });
@@ -128,33 +165,6 @@ $(document).ready(function () {
 </script>
 
 <style scoped>
-/* #app {
-font-family: Avenir, Helvetica, Arial, sans-serif;
--webkit-font-smoothing: antialiased;
--moz-osx-font-smoothing: grayscale;
-text-align: center;
-color: #2c3e50;
-margin-top: 60px;
-}
-.left-align {
-	text-align: left;
-	background-color: #fafafa;
-	padding: 10px;
-	margin-bottom: 10px;
-}
-.is-20px {
-  width: 20px;
-}
-.container {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-  grid-gap: 0.5em;
-}
-.productGrid {
-	padding: 10px;
-} */
-
-
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
